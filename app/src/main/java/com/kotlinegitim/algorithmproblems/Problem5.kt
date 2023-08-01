d4d58c659905917567a7bd51417e0a7d3a33da50
emrekirik <mertkirik46@gmail.com>
2023-08-01 21:26:12 +0300

    Bir listede tekrar edilen sayıları döndürme

--- app/src/main/java/com/kotlinegitim/algorithmproblems/Problem5.kt
+++ app/src/main/java/com/kotlinegitim/algorithmproblems/Problem5.kt
@@ -3,12 +3,19 @@ package com.kotlinegitim.algorithmproblems
 fun main() {
         //Verilen bir listedeki tekrar eden elemanları HashSet olarak döndüren bir fonksiyon yazın.
     val list = listOf<Int>(5,7,3,7,8,3)
-    println("Normal liste : " + list)
-    val duzeltilmisListe = hashSetListe(list)
-    println("Düzeltilmiş Liste : " + duzeltilmisListe)
+    println("Tekrar eden : " + hashSetListe(list))
 
 }
 fun hashSetListe(list: List<Int>): HashSet<Int> {
-    val duzeltilmisListe = list.toSet().toHashSet()
-    return duzeltilmisListe
+
+    val tekrarEden = HashSet<Int>()
+    val tekrarEtmeyen = HashSet<Int>()
+
+    for (i in list){
+        if (!tekrarEtmeyen.add(i)){
+            tekrarEden.add(i)
+        }
+    }
+    return tekrarEden
+
 }
