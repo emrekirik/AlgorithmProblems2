a43fd31f932d08572e8eb4ff8b3738ce5b7023e6
emrekirik <mertkirik46@gmail.com>
2023-08-01 22:11:38 +0300

    Bir SetOf'taki elemanları diğer bir SetOf'tan çıkaran bir fonksiyon

---
+++ app/src/main/java/com/kotlinegitim/algorithmproblems/Problem9.kt
@@ -0,0 +1,13 @@
+package com.kotlinegitim.algorithmproblems
+
+fun main() {
+    //Bir SetOf'taki elemanları diğer bir SetOf'tan çıkaran bir fonksiyon yazın
+    val list = setOf<Int>(1,2,3,4,5,6)
+    val list2 = setOf<Int>(1,2,3)
+    val cikarma = cikarma(list, list2)
+    println(cikarma)
+
+}
+fun <T> cikarma(set1: Set<T>, set2: Set<T>): Set<T> {
+    return set1 - set2
+}
