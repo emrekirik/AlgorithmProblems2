913663b0721e1842c63dd3cf30b1beb3d1a474bf
emrekirik <mertkirik46@gmail.com>
2023-08-01 21:37:09 +0300

    HashSet Problemi

---
+++ app/src/main/java/com/kotlinegitim/algorithmproblems/Problem6.kt
@@ -0,0 +1,17 @@
+package com.kotlinegitim.algorithmproblems
+
+fun main() {
+    //Bir HashSet'in belirli bir elemanı içerip içermediğini kontrol eden bir fonksiyon yazın.
+    val list = hashSetOf(1,2,3,5,8,6)
+    val secilenEleman = 8
+    val belirtilenEleman = belirtilenElmaniAl(list,secilenEleman)
+    if (belirtilenEleman == true) {
+        println("Seçilen eleman listede var : " + secilenEleman )
+
+    }
+}
+
+fun belirtilenElmaniAl(set: HashSet<Int>, element: Int): Boolean{
+    return set.contains(element)
+
+}
